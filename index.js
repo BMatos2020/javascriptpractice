1277ce35aecca7ab3aa6c0efdc1eefaab42a2bb5
BMatos2020 <brenda.matosux@gmail.com>
2020-01-28 00:54:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,134 @@
+// let x = 5
+
+// console.log(x)
+
+// console.log('Demonstrating a string.')
+
+// let y = true
+
+// console.log(y)
+
+// console.log('y is a boolean data type')
+
+// let z = [1,2,3, true, 5, 'hello', ['t',9]]
+
+// console.log(z)
+
+// console.log('z is an array with different data types. To access items, you need to index them beginning at 0.  Z has an index of 0-6.')
+
+// console.log( z[5], 'to access list item 6')
+
+// console.log( 'concetonating strings means to combine:', x + z[2])
+
+// for (let index in z){
+//   console.log(index, 'the entire array')
+// }
+
+// for (let index in z){
+//   console.log(z[index])
+// }
+
+// for (let i=0; i < z.length; i++) {
+//   console.log (i)
+// }
+
+//key value pairs
+
+// x = {
+//   one: 1,
+//   two: 2,
+//   name: 'John',
+//   age: false
+// }
+
+// y = [1,2,3,4, [], {}]
+
+// console.log (x)
+
+// console.log ( x['name'] )
+
+// console.log( y[2])
+
+//Exercises 1/27/20
+                      
+//console.log the string 'hello world
+
+console.log('hello world')
+
+// save num 7 in a var and console.log item
+
+let a = 7
+
+console.log(a)
+
+// x = 5 
+
+let x = 5
+
+// create a var set it equal to x plus 10, console.log it
+
+var y = x + 10
+
+console.log(y)
+
+// create var set it equal to 'hello', create another equal to 'world'.. concatenate them with a space in the middle and console.log it
+
+var stringOne = 'Hello'
+var stringTwo = 'World'
+
+console.log(stringOne + ' ' + stringTwo)
+
+// use x = [9,8,7,6]
+
+// console.log num 8 from the list x
+
+//console.log num 6 from the list x
+
+let z = [9,8,7,6]
+
+console.log(z[1])
+console.log(z[3])
+
+
+// use obj = {
+//   name: 'Joe',
+//   age: 5
+// }
+
+// console.log 'Joe" from the var obj
+
+// console.log 5 from the var obj
+
+let obj = {
+  name: 'Joe',
+  age: 5
+}
+
+console.log(obj.name)
+console.log(obj.age)
+
+// google how to create a random number with javascript
+
+let any = Math.random()
+
+console.log(any)
+
+// use that random number to get a random item from the list x used above, ask if lost 
+
+// function random_item(any)
+// {
+  
+// return any[Math.floor(Math.random()*any.length)];
+     
+// }
+
+
+var rand = z[Math.floor(Math.random() * z.length)];
+
+console.log(rand)
+
+
+
+
+
+
